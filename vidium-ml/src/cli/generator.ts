a7063e2c84d90bd2ccc4cd4241577525fb83ce80
Ivan Vdt <ivan.van-der-tuijn@etu.unice.fr>
2025-01-05 16:28:26 +0100

    Time abstraction code generation for Text, Image and Video

--- vidium-ml/src/cli/generator.ts
+++ vidium-ml/src/cli/generator.ts
@@ -24,7 +24,7 @@ function compile(video: Video, fileNode: CompositeGeneratorNode): void {
 
     // Create composition
     fileNode.append('# Create composition', NL);
-    fileNode.append('scene = mv.layer.Composition(size=(1920, 1080))', NL, NL);
+    fileNode.append('scene = mv.layer.Composition(size=(1920, 1080), duration=5.0)', NL, NL);
 
     // Process elements
     generateElements(video.elements, fileNode);
@@ -54,17 +54,13 @@ function generateElements(elements: AssetElement[], fileNode: CompositeGenerator
                 generateAssetItem(element.item, varName, fileNode);
                 fileNode.append(`scene.add_layer(${varName})`, NL);
                 break;
-
-            case 'Rectangle':
-            case 'Clip':
-            case 'Image':
-            case 'Text':
+            case 'UseAsset':
+                // TODO: Implement UseAsset, to generate the same code as DefineAsset.item
+                break;
+            default:
                 // Direct AssetItem cases
                 generateAssetItem(element, varName, fileNode);
-                fileNode.append(`scene.add_layer(${varName})`, NL);
-                break;
-
-            default:
+                fileNode.append(`scene.add_layer(${varName}_item)`, NL);
                 break;
         }
     });
@@ -83,36 +79,49 @@ function generateAssetItem(item: AssetItem, varName: string, fileNode: Composite
         case 'Clip':
             const clip = item as Clip;
             fileNode.append(`${varName} = mv.layer.Video("${clip.path}")`, NL);
-            if (clip.from !== undefined || clip.to !== undefined) {
-                fileNode.append(`${varName} = mv.trim(${varName}, `);
-                fileNode.append(`start_times=[${clip.from ?? 0}], `);
-                fileNode.append(`end_times=[${clip.to ?? `${varName}.duration`}])`, NL);
-            }
+            compileTime(clip.from, clip.to, varName, fileNode);
             break;
 
         case 'Image':
             const img = item as Image;
             fileNode.append(`${varName} = mv.layer.Image("${img.path}")`, NL);
+            compileTime(img.from, img.to, varName, fileNode);
             break;
 
         case 'Text':
-            const text = item as Text;
-            fileNode.append(`${varName} = mv.layer.Text("${text.text}")`, NL);
+            const txt = item as Text;
+            const text = txt.text ? txt.text : '';
+            const color = txt.color ? compileColor(txt.color) : "#ffffff";
+            const font_size = txt.size ? `${txt.size}` : 30;
+            fileNode.append(`${varName} = mv.layer.Text("${text}", font_size=${font_size}, color="${color}")`, NL);
+            compileTime(txt.from, txt.to, varName, fileNode);
             break;
     }
 }
 
+function compileTime(from: number | undefined, to: number | undefined, varName: string, fileNode: CompositeGeneratorNode): void {
+    const hasFrom = from !== undefined;
+    const hasTo = to !== undefined;
+
+    if (hasFrom || hasTo) {
+        const start = hasFrom ? from : 0;
+        const end = hasTo ? to : `${varName}.duration`;
+        fileNode.append(`${varName}_item = mv.layer.LayerItem(${varName}, offset=${start}, start_time=0.0, end_time=${end})`, NL);
+    } else {
+        fileNode.append(`${varName}_item = mv.layer.LayerItem(${varName})`, NL);
+    }
+}
 function compileColor(color: string): string {
     // Remove quotes from color string
     const cleanColor = color.replace(/['"]+/g, '').toUpperCase();
     switch (cleanColor) {
         case 'RED':
-            return '(255, 0, 0)';
+            return '#ff0000';
         case 'GREEN':
-            return '(0, 255, 0)';
+            return '#00ff00';
         case 'BLUE':
-            return '(0, 0, 255)';
+            return '#0000ff';
         default:
-            return '(0, 0, 0)';
+            return '#000000';
     }
 }
