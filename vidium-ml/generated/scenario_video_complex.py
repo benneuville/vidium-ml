4d53a89ab690b74205bda61e90f55437d9d07c02
Ivan Vdt <ivan.van-der-tuijn@etu.unice.fr>
2025-01-18 16:59:39 +0100

    fix ID

--- vidium-ml/generated/scenario_video_complex.py
+++ vidium-ml/generated/scenario_video_complex.py
@@ -22,16 +22,12 @@ element_3_transform = mv.Transform(position=(video_width/2, video_height/2), sca
 scene.add_layer(element_3, name="element_3", transform=element_3_transform , offset=1, start_time=0.0, end_time=2)
 
 element_4 = mv.layer.Text("COUCOU", font_size=50, color="#ff0000")
-element_4_width = element_4.get_size()[0]
-element_4_height = element_4.get_size()[1]
-element_4_transform = mv.Transform(position=(video_width/2, video_height/2), scale=(2, 2), rotation=0, opacity=0.3)
-scene.add_layer(element_4, name="element_4", transform=element_4_transform , offset=2, start_time=0.0, end_time=2)
+element_4_transform = mv.Transform(position=(960, 250), scale=(1.0, 1.0), rotation=0, opacity=1)
+scene.add_layer(element_4, name="element_4", transform=element_4_transform , offset=4, start_time=0.0, end_time=0)
 
 element_5 = mv.layer.Text("COUCOU", font_size=50, color="#ff0000")
-element_5_width = element_5.get_size()[0]
-element_5_height = element_5.get_size()[1]
-element_5_transform = mv.Transform(position=(video_width/2, video_height/2), scale=(2, 2), rotation=0, opacity=0.3)
-scene.add_layer(element_5, name="element_5", transform=element_5_transform , offset=2, start_time=0.0, end_time=2)
+element_5_transform = mv.Transform(position=(960, 250), scale=(1.0, 1.0), rotation=0, opacity=1)
+scene.add_layer(element_5, name="element_5", transform=element_5_transform , offset=4, start_time=0.0, end_time=0)
 
 
 # Export video
